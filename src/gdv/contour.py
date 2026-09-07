471cac14138cee7e988b4dce27e07a5ac7ee3124
Kenneth O'Dell <kenneth.l.odell@gmail.com>
2026-09-06 20:50:57 -0400

    plot: added plotting function for the objective function only

--- src/gdv/contour.py
+++ src/gdv/contour.py
@@ -4,9 +4,56 @@ from matplotlib.figure import Figure
 from numpy.typing import NDArray
 from typing import Iterable
 
-#TODO: implement plain visualization for just the contour. Keep it as separate func.
-def func_contour():
-    pass
+# plotting func for the objective function only.
+def func_contour(
+    X: NDArray,
+    Y: NDArray,
+    Z: NDArray,
+
+    figsize: tuple[int] = (10,7),
+    num_contour_levels: int = 25,
+    contourf_cmap: str = 'coolwarm',
+    contourf_alpha: float = 0.7,
+    contourf_label: str = 'Objective Value (Z)',
+
+    contour_colors: str = 'black',
+    contour_linewidths: float = 0.5,
+
+    legend: bool = True
+
+    ) -> Figure:
+    """
+    Creates a contour plot from a discrete grid of the search space.
+
+    Requires an X, Y, Z 1D-array input where X,Y denote the coordinates in the search space,
+    and Z is the objective function evaluated at (X, Y).
+    """
+    # instantiate a new figure for the contour plot
+    fig, ax = plt.subplots(figsize=figsize)
+
+    # creates the color fill for contour levels
+    contour_filled = ax.contourf(
+        X, Y, Z, 
+        cmap=contourf_cmap,  
+        alpha=contourf_alpha,
+        levels=num_contour_levels
+    )
+
+    # adds a colorbar to the contour plot with a label
+    fig.colorbar(contour_filled, label=contourf_label)
+
+    # draws countour level boundaries on top of the filled contours
+    ax.contour(
+        X, Y, Z,
+        levels=num_contour_levels,
+        colors=contour_colors,
+        linewidths=contour_linewidths
+    )
+
+    if legend:
+        ax.legend()
+
+    return fig
 
 # primary func for generating the contours + historically travelled path.
 def optim_contour(
